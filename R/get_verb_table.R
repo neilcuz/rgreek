b087ea291e7c906bb71130d19b3ad9ca8d980175
neilcuz <neil.george.currie@gmail.com>
2023-02-08 16:10:42 +0000

    Edited verbs code to handle when the verb table is not in the first table on the wiktionary page

--- R/get_verb_table.R
+++ R/get_verb_table.R
@@ -1,13 +1,38 @@
 
+
+# # Standard case
+# english_verb <- "upgrade"
+# greek_verb <- "αναβαθμίζω"
+#
+#
+# # Edge case
+
+# english_verb_tenses <- c("signed",
+#                          "will sign",
+#                          "was signing",
+#                          "will be signing",
+#                          "had signed",
+#                          "have signed",
+#                          "will have signed")
+
+
 get_verb_table <- function (english_verb, greek_verb, english_verb_tenses) {
 
   wiktionary_url <- paste0("https://el.wiktionary.org/wiki/", greek_verb)
 
-  declension_raw_tbl <- wiktionary_url |>
+  wiktionary_tables <- wiktionary_url |>
     read_html() |>
     html_elements("table") |>
-    html_table() |>
-    extract2(1) |>
+    html_table()
+
+  # Find verb table number
+
+  table_num <- .find_verb_table_num(wiktionary_tables)
+
+  # And pull it out
+
+  declension_raw_tbl <- wiktionary_tables |>
+    extract2(table_num) |>
     clean_names()
 
   # Extract data from the raw tables and build the words
@@ -51,4 +76,29 @@ clean_verb <- function (x) {
 
 }
 
+.find_verb_table_num <- function (wiktionary_tables) {
+
+  num_tables <- length(wiktionary_tables)
+
+  # This word relates to verb tenses
+
+  match_col_name <- "Εξακολουθητικοί χρόνοι"
+
+  for (i in 1:num_tables) {
+
+    col_names <- colnames(wiktionary_tables[[i]])
+
+    if (match_col_name %in% col_names)  break
+
+    # No table found will class as indelcinable. Add 1 to i . When we use i
+    # in the main function to figure out the position, i will be bigger than
+    # the length so we known no table was found i.e. indeclinable.
+
+    if (i == num_tables) i <- i + 1
+
+  }
+
+  return (i)
+
+}
 
--- run/process-verbs.R
+++ run/process-verbs.R
@@ -2,18 +2,27 @@
 suppressPackageStartupMessages(devtools::load_all())
 message("Processing Verbs...")
 
-verbs_to_do <- "~/OneDrive/Greek/greek_to_do.xlsx" |>
-  read_excel(sheet = "verbs") |>
+verbs_to_do_raw <- read_excel("~/OneDrive/Greek/greek_to_do.xlsx",
+                              sheet = "verbs")
+
+verbs_to_do <- verbs_to_do_raw |>
   clean_names() |>
   nest(tenses = past_simple:future_perfect) |>
   mutate(tenses = map(tenses, unlist))
 
-verbs_table <- list(english_verb = verbs_to_do$english_verb,
-     greek_verb = verbs_to_do$greek_verb,
-     english_verb_tenses = verbs_to_do$tenses) |>
-  pmap_dfr(possibly(get_verb_table, otherwise = NULL))
+suppressWarnings(
+  verbs_table <- list(english_verb = verbs_to_do$english_verb,
+                      greek_verb = verbs_to_do$greek_verb,
+                      english_verb_tenses = verbs_to_do$tenses) |>
+    pmap_dfr(possibly(get_verb_table, otherwise = NULL))
+)
+
 
 suppressWarnings(closeAllConnections())
+
+
+
+
 
 # Split the verbs into present tense; past and future tenses simple and
 # continuous; perfect tenses
@@ -32,7 +41,20 @@ verbs_table_perfect <- verbs_table |>
 
 # Add code for error
 
+to_do <- verbs_to_do$english_verb
+done <- verbs_table_present$english_verb
 
+verbs_table_error <- tibble(english_verb = to_do[!(to_do %in% done)]) |>
+  left_join(verbs_to_do_raw, by = "english_verb") |>
+  distinct(.keep_all = TRUE)
+
+# User message
+
+summary_message <- glue("Complete: {nrow(verbs_table_present)}\n",
+                        "Error: {nrow(verbs_table_error)}")
+
+print(summary_message)
+View(verbs_table_present)
 
 # Output
 
@@ -43,6 +65,4 @@ write_csv(verbs_table_present, filenames[1], col_names = FALSE)
 write_csv(verbs_table_past_future, filenames[2], col_names = FALSE)
 write_csv(verbs_table_perfect, filenames[3], col_names = FALSE)
 write_csv(verbs_table_error, filenames[4], col_names = FALSE)
-
-write_csv(incomplete, filenames[5])
 
